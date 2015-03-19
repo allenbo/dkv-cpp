6bce3f208c518f1435bc2073669d883878b98ae6
abo <linjianfengqrh@gmail.com>
2015-03-19 03:26:10 -0400

    Update client file to monitor time

--- app/client_bin.cpp
+++ app/client_bin.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
+#include "sys/time.h"
 
 using dkv::Client;
 using dkv::Status;
@@ -33,7 +34,12 @@ std::string rand_string(int len) {
 }
 
 
+float diff_time(struct timeval start, struct timeval end) {
+  return end.tv_sec - start.tv_sec + (end.tv_usec - start.tv_usec) / 1000000.0;
+}
+
 int main(int argc, char** argv) {
+  struct timeval start, end;
   Client clt("127.0.0.1:12321");
 
   std::vector<std::string> keys;
@@ -42,52 +48,78 @@ int main(int argc, char** argv) {
   std::string key, value;
   Status st;
   // test for set
+  std::cout << "Start set operations" << std::endl;
+  gettimeofday(&start, nullptr);
   for(int i = 0; i < MAJOR_TEST_SIZE; i ++ ) {
     key = rand_string(rand() % (MAX_KEY_SIZE - MIN_KEY_SIZE ) + MIN_KEY_SIZE);
     value = rand_string(rand() % (MAX_VALUE_SIZE - MIN_VALUE_SIZE ) + MIN_VALUE_SIZE);
     keys.push_back(key);
     values.push_back(value);
-    std::cout << "Set [" << key << "| value ]" << std::endl;
+    //std::cout << "Set [" << key << "| value ]" << std::endl;
 
     st = clt.set(key, value);
     LOG_IF(FATAL, !st.ok());
   }
+  std::cout << "End set operations" << std::endl;
+  gettimeofday(&end, nullptr);
+  std::cout << "Set operations take " << diff_time(start, end) << "s" << std::endl;
 
   // test for get
+  std::cout << "Start get operations" << std::endl;
+  gettimeofday(&start, nullptr);
   for(int i = 0; i < MAJOR_TEST_SIZE; i ++ ) {
     key = keys[i];
-    std::cout << "Get value for key [" << key << "]" << std::endl;
+    //std::cout << "Get value for key [" << key << "]" << std::endl;
     st = clt.get(key, &value);
     assert(st.ok());
     LOG_IF(FATAL, !st.ok());
     LOG_IF(FATAL, value != values[i]);
   }
+  std::cout << "End of get operations" << std::endl;
+  gettimeofday(&end, nullptr);
+  std::cout << "Get operations take " << diff_time(start, end) << "s" << std::endl;
 
   // test for update
+  std::cout << "Start update operations" << std::endl;
+  gettimeofday(&start, nullptr);
   for(int i = 0; i < MAJOR_TEST_SIZE; i ++ ) {
     values[i] = rand_string(rand() % (MAX_VALUE_SIZE - MIN_VALUE_SIZE ) + MIN_VALUE_SIZE);
-    std::cout << "Update [" << keys[i] << "| value ]" << std::endl;
+    //std::cout << "Update [" << keys[i] << "| value ]" << std::endl;
     st = clt.update(keys[i], values[i]);
     LOG_IF(FATAL, !st.ok());
   }
+  std::cout << "End of update operations" << std::endl;
+  gettimeofday(&end, nullptr);
+  std::cout << "Update operations take " << diff_time(start, end) << "s" << std::endl;
 
+  std::cout << "Start get operations" << std::endl;
+  gettimeofday(&start, nullptr);
   for(int i = 0; i < MAJOR_TEST_SIZE; i ++ ) {
     key = keys[i];
-    std::cout << "Get value for key [" << key << "]" << std::endl;
+    //std::cout << "Get value for key [" << key << "]" << std::endl;
     st = clt.get(key, &value);
     assert(st.ok());
     LOG_IF(FATAL, !st.ok());
     LOG_IF(FATAL, value != values[i]);
   }
+  std::cout << "End of get operations" << std::endl;
+  gettimeofday(&end, nullptr);
+  std::cout << "Get operations take " << diff_time(start, end) << "s" << std::endl;
 
   // test for del
+  std::cout << "Start del operations" << std::endl;
+  gettimeofday(&start, nullptr);
   for(int i = 0; i < MAJOR_TEST_SIZE; i ++ ) {
     key = keys[i];
-    std::cout <<"Delete k/v [" << key << "]" << std::endl;
+    //std::cout <<"Delete k/v [" << key << "]" << std::endl;
     st = clt.del(key);
     LOG_IF(FATAL, !st.ok());
   }
+  std::cout << "End of del operations" << std::endl;
+  gettimeofday(&end, nullptr);
+  std::cout << "Del operations take " << diff_time(start, end) << "s" << std::endl;
 
+  /*
   // test for sets
   std::vector<std::string> batch_keys;
   std::vector<std::string> batch_values;
@@ -113,5 +145,6 @@ int main(int argc, char** argv) {
       batch_values.clear();
     }
   }
+  */
   
 }
